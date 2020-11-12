29d178a8b7354d6deb6c013aa92b6304bfc15adb
Atai <ataibotaliev@gmail.com>
2020-11-12 16:28:34 +0600

    add 'about' and 'clients'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-$('.hero__slider').slick({
+$('.slider').slick({
     infinite: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    dots:true
   });
+
+$('.clients__items').slick({
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidestoScroll:1,
+    dots:true
+});
