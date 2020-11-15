f6d89538dfcd9f4cd9ef59ed77884ff035336c68
Atai <ataibotaliev@gmail.com>
2020-11-15 22:57:12 +0600

    add more animations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// ================= slick
 $('.hero__items').slick({
     infinite: true,
     speed: 1000,
@@ -21,50 +22,158 @@ $('.reviews__items').slick({
   slidestoScroll:1,
   dots:true
 });
-// gsap animation
-TweenMax.from('.header__logo', 1.4, {
+
+// ================= animations
+TweenMax.from('.subtitle', 1, {
   delay: 0.1,
   x: "-100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.header__item', 1.4, {
+// header
+TweenMax.from('.header__logo', 1.4, {
   delay: 0.2,
-  y: "-50",
+  x: "-100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.header__nav', 1.6, {
+TweenMax.from('.header__first', 1.2, {
+  delay: 0.2,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.header__second', 1.2, {
   delay: 0.4,
-  y: "50",
+  opacity: 0,
+  x: "100%",
   ease: Expo.easeInOut,
 });
+TweenMax.from('.header__third', 1.2, {
+  delay: 0.6,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.header__fourth', 1.2, {
+  delay: 0.8,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+
+// nav
+TweenMax.from('.nav__first', 1, {
+  delay: 0.3,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.nav__second', 1, {
+  delay: 0.4,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.nav__third', 1, {
+  delay: 0.5,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.nav__fourth', 1, {
+  delay: 0.7,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.nav__fifth', 1, {
+  delay: 0.7,
+  opacity: 0,
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+
+// hero
 TweenMax.from('.hero__slider', 1.6, {
   delay: 0.1,
   x: "-100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.subtitle', 1.3, {
+
+// services
+TweenMax.from('.services__title', 1, {
+  delay: 0.1,
+  opacity: 0,
+  x: "-100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__first', 1, {
+  delay: 0.1,
+  opacity: 0,
+  y: "-100",
+  x: "-100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__fourth', 1, {
+  delay: 0.1,
+  opacity: 0,
+  y: "100",
+  x: "-100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__third', 1, {
+  delay: 0.1,
+  opacity: 0,
+  y: "-100",
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__sixth', 1, {
+  delay: 0.1,
+  opacity: 0,
+  y: "100",
+  x: "100%",
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__second', 1, {
+  delay: 0.3,
+  opacity: 0,
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.service__fifth', 1, {
+  delay: 0.3,
+  opacity: 0,
+  ease: Expo.easeInOut,
+});
+
+// about
+TweenMax.from('.about__title', 1.2, {
   delay: 0.2,
-  x: "-100vw",
+  x:"-100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.services__title', 1.5, {
+TweenMax.from('.about__description', 1.4, {
   delay: 0.4,
-  x: "-100vw",
+  x:"-100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.services__items img', 1.5, {
+TweenMax.from('.about__down', 1.1, {
+  delay: 0.3,
+  opacity: 0,
+  y: '-200',
+  ease: Expo.easeInOut,
+});
+TweenMax.from('.about__up', 1.1, {
+  delay: 0.2,
+  opacity: 0,
+  y: '200',
+  ease: Expo.easeInOut,
+});
+
+// clients
+TweenMax.from('.clients__title', 1.3, {
   delay: 0.1,
   opacity: 0,
-  ease: Expo.easeInOut,
-});
-TweenMax.from('.services__items span', 1.5, {
-  delay: 0.1,
-  opacity: 0,
-  ease: Expo.easeInOut,
-});
-TweenMax.from('.clients__title', 1.5, {
-  delay: 0.1,
-  x: "-100vw",
+  x: "-100%",
   ease: Expo.easeInOut,
 });
 TweenMax.from('.clients__slider', 1.5, {
@@ -72,31 +181,20 @@ TweenMax.from('.clients__slider', 1.5, {
   opacity: 0,
   ease: Expo.easeInOut,
 });
-TweenMax.from('.reviews__title', 1.5, {
+
+// rewiews
+TweenMax.from('.reviews__title', 1.3, {
   delay: 0.1,
-  x: "-100vw",
+  opacity: 0,
+  x: "-100%",
   ease: Expo.easeInOut,
 });
 TweenMax.from('.reviews__slider', 1.5, {
   delay: 0.1,
-  opacity: 0,
+  x: "100vw",
   ease: Expo.easeInOut,
 });
-TweenMax.from('.about__left .about__title', 1, {
-  delay: 0.2,
-  x:"-100vw",
-  ease: Expo.easeInOut,
-});
-TweenMax.from('.about__left .about__description', 1, {
-  delay: 0.4,
-  x:"-100vw",
-  ease: Expo.easeInOut,
-});
-TweenMax.from('.about__right .about__items', 1, {
-  delay: 0.4,
-  opacity: 0,
-  ease: Expo.easeInOut,
-});
+
 
 
 // setTimeout(timer, 500);
